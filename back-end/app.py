314a69cf666f4194e719d9feb863ade1b50b3997
Lar <so_orr@mail.ru>
2019-11-29 12:48:19 +0100

    CORS fixed

--- back-end/app.py
+++ back-end/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 # TODO put it to another file and make other classes
 class Address:
