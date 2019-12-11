2d311028741ee23495d08ef337e4fab66690418a
Lar <so_orr@mail.ru>
2019-12-11 18:32:31 +0100

    Database fix

--- back-end/database/db_reset.py
+++ back-end/database/db_reset.py
@@ -9,7 +9,7 @@ db.create_all()
 places = [
     {'name': 'KFS', 'type': 'fast food', 'address':
         {'street': 'Greenstraat', 'house_number': '2', 'postcode': '9321CV', 'city': 'Groningen', 'country': 'Netherlands'},
-     'total_seats': 20, 'free_seats': 10},
+     'total_seats': 20, 'free_seats': 10, 'username': 'user1', 'password': 'password1'},
     {'name': 'Burgers Queen', 'type': 'restaurant', 'address':
         {'street': 'Redstraat', 'house_number': '3a', 'postcode': '9821CV', 'city': 'Amsterdam', 'country': 'Netherlands'},
      'total_seats': 120, 'free_seats': 75},
