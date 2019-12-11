07a0ee15cf8088819e1789d7b662d57bd6c964af
richardswesterhof <richardswesterhof@gmail.com>
2019-12-11 13:47:09 +0100

    fixed mobile friendliness of login page

--- front-end/src/components/RestaurantView/RestaurantAuthenticationPage.vue
+++ front-end/src/components/RestaurantView/RestaurantAuthenticationPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="columns">
-    <div class="column is-4 is-offset-1">
+  <div class="columns is-mobile">
+    <div class="column is-5 is-hidden-touch is-offset-1">
       <h2>{{$props.reasonMessage}}</h2>
       <b-field label="username">
         <b-input placeholder="username"></b-input>
@@ -15,13 +15,38 @@
           <b-button class="button is-primary" @click="authenticate" style="display: inline-block;">log in</b-button>
         </div>
 
-        <div class="column has-text-right">
+        <div class="column has-text-right is-mobile">
           <router-link to="/register"><p style="margin: 1% 1% 0 0; display:inline-block;">I don't have a restaurant account yet</p></router-link>
         </div>
       </div>
 
     </div>
 
+    <div class="column is-10 is-hidden-desktop is-offset-1">
+      <h2>{{$props.reasonMessage}}</h2>
+      <b-field label="username">
+        <b-input placeholder="username"></b-input>
+      </b-field>
+
+      <b-field label="password">
+        <b-input type="password" placeholder="password" password-reveal></b-input>
+      </b-field>
+
+      <div class="columns is-mobile">
+        <div class="column is-1">
+          <b-button class="button is-primary" @click="authenticate" style="display: inline-block;">log in</b-button>
+        </div>
+
+        <div class="column has-text-right">
+          <router-link to="/register">
+            <p class="is-hidden-mobile" style="margin: 1% 1% 0 0; display:inline-block;">I don't have a restaurant account yet</p>
+            <p class="is-hidden-tablet">Register</p>
+          </router-link>
+        </div>
+      </div>
+    </div>
+
+    
     <b-loading :active="isLoading"></b-loading>
   </div>
 </template>
