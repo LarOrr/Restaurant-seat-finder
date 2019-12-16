b1c190fa96b035fcd76da9ea6eaee65d15e94a0a
Lar <so_orr@mail.ru>
2019-12-16 15:51:19 +0100

    Deletion endpoint and README

--- back-end/database/db_reset.py
+++ back-end/database/db_reset.py
@@ -9,7 +9,9 @@ db.create_all()
 places = [
     {'name': 'KFS', 'type': 'fast food', 'address':
         {'street': 'Greenstraat', 'house_number': '2', 'postcode': '9321CV', 'city': 'Groningen', 'country': 'Netherlands'},
-     'total_seats': 20, 'free_seats': 10, 'username': 'user1', 'password': 'password1'},
+     'total_seats': 20, 'free_seats': 10, 'username': 'user1', 'password': 'password1',
+     'website': 'https://www.KSFsuperwebsite3000.world', 'email': 'contact@ksf.world', 'phone_number': "+123456789",
+     'description': 'We have good food!'},
     {'name': 'Burgers Queen', 'type': 'restaurant', 'address':
         {'street': 'Redstraat', 'house_number': '3a', 'postcode': '9821CV', 'city': 'Amsterdam', 'country': 'Netherlands'},
      'total_seats': 120, 'free_seats': 75},
